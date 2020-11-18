7d2197a9900130dce01f6b85c15db8ba4594b2ff
dralexsand <dr.alex.sand.111@gmail.com>
2020-11-19 00:58:19 +0600

    configure RouteServiceProvider for api, web, user, admin groups routes

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\server\domains\projectss\cars.loc\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\server\domains\projectss\cars.loc\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\server\domains\projectss\cars\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\server\domains\projectss\cars\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
